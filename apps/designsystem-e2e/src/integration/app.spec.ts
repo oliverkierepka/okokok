fc0d4a8b549d2020dc307f915f7e0f262ec9ffd4
Oliver Kierepka <oliver.kierepka@rhenag.de>
2022-04-11 11:28:37 +0200

    DS: add prettier, stylelint, dsp

--- apps/designsystem-e2e/src/integration/app.spec.ts
+++ apps/designsystem-e2e/src/integration/app.spec.ts
@@ -1,13 +1,10 @@
 import { getGreeting } from '../support/app.po';
 
 describe('designsystem', () => {
-  beforeEach(() => cy.visit('/'));
+	beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome designsystem');
-  });
+	it('should display welcome message', () => {
+		// Function helper example, see `../support/app.po.ts` file
+		getGreeting().contains('Welcome designsystems');
+	});
 });
